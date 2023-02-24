807a172d8e8b27a7e9f6f60074531acff89d3cec
HAI <HAI@DESKTOP-R7PD6US>
2023-02-24 14:03:07 +0530

    Add project files.

---
+++ Temperature converter/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Temperature_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double c, f, k;
+
+            if (radioButton1.Checked == true)
+            {
+                c = double.Parse(textBox2.Text);
+                f = (c + 9 / 5) + 32;
+                textBox1.Text = Convert.ToString(f);
+
+            }
+            if (radioButton2.Checked == true)
+            {
+                f = double.Parse(textBox2.Text);
+                c = (f - 32) + 5 / 9;
+                textBox1.Text = Convert.ToString(c);
+
+            }
+            if (radioButton4.Checked == true)
+            {
+                c = double.Parse(textBox2.Text);
+                k = c + 273;
+                textBox1.Text = Convert.ToString(k);
+
+            }
+            if (radioButton4.Checked == true)
+            {
+                k = double.Parse(textBox2.Text);
+                c = k - 273;
+                textBox1.Text = Convert.ToString(c);
+            }
+        }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+            MessageBox.Show("Thank You");
+                Application.Exit();
+            }
+        }
+    }
+
+
+
---
+++ Temperature converter/Measurement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature_converter
+{
+    public partial class Measurement : Form
+    {
+        public Measurement()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox2.Text == "Select Unit" || comboBox1.Text == "Select Unit")
+            {
+                MessageBox.Show("Please select Unit for conversion");
+            }
+            else
+            {
+                if(comboBox2.SelectedItem == "Kilometer" && comboBox1.SelectedItem == "Meter")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * 1000);
+                }
+                if (comboBox2.SelectedItem == "Meter" && comboBox1.SelectedItem == "Kilometer")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * .001);
+                }
+                if (comboBox2.SelectedItem == "Kilometer" && comboBox1.SelectedItem == "Centimeter")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * 100000);
+                }
+                if (comboBox2.SelectedItem == "Centimeter" && comboBox1.SelectedItem == "Kilometer")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * .0001);
+                }
+                if (comboBox2.SelectedItem == "Centimeter" && comboBox1.SelectedItem == "Meter")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * .001);
+                }
+                if (comboBox2.SelectedItem == "Meter" && comboBox1.SelectedItem == "Centimeter")
+                {
+                    textBox1.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) * 100);
+                }
+            }
+        }
+    }
+}
